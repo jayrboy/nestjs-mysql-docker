67c47eef5f5ab37374829be8bd8531b23b8695fb
jarboy <j.jayox_@hotmail.com>
2024-08-05 19:15:54 +0700

    Add CRUD Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/users/users.module';
+import { User } from './modules/users/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'mysql-server',
+      host: 'mysql-server', // localhost
       port: 3306,
       database: 'nestjs_db',
       username: 'root',
       password: '1234',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
