99c2339f82785c915f7c02ebcebbccafc6987beb
jarboy <j.jayox_@hotmail.com>
2024-08-05 16:59:28 +0700

    Add Docker Compose MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql-server',
+      port: 3306,
+      database: 'nestjs_db',
+      username: 'root',
+      password: '1234',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
